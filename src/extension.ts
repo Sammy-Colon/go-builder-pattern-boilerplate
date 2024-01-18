0d32cf98ed896f0898855a6284c0cba27d8e79da
Sammy-Colon <sammy.colon@posteo.me>
2024-01-18 07:47:13 +0100

    feat: add support for struct fields like 'A, B, C string'

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
 function generateBoilerplate() {
 	const editor = vscode.window.activeTextEditor;
 
-	if (editor == null) {
+	if (editor === undefined) {
 		return;
 	}
 
@@ -34,21 +34,27 @@ function generateBoilerplate() {
 		.map(line => line.trim().replace(/\s+/g, ' '));
 
 	if (!lines[0].startsWith("type") || !lines[0].endsWith("struct {") || lines[lines.length - 1].trim() !== "}") {
-		vscode.window.showErrorMessage("You didn't select a Golang struct!")
+		vscode.window.showErrorMessage("You didn't select a Golang struct!");
 		return;
 	}
 
 	const structName = lines[0].split(" ")[1].trim();
 
 	editor.edit(editBuilder => {
-		editBuilder.insert(selection.end, `\n${generateOptionType(structName)}`)
+		editBuilder.insert(selection.end, `\n${generateOptionType(structName)}`);
 
 		for (const line of lines.slice(1, -1)) {
-			const [attribute, dataType] = line.split(" ");
-			editBuilder.insert(selection.end, generateWithFunction(structName, attribute, dataType))
+			const lastSpaceIndex = line.lastIndexOf(" ");
+			const dataType = line.substring(lastSpaceIndex + 1);
+			const attributeString = line.substring(0, lastSpaceIndex);
+			const attributes = attributeString.split(",").map(attribute => attribute.trim());
+
+			for (const attribute of attributes) {
+				editBuilder.insert(selection.end, generateWithFunction(structName, attribute, dataType));
+			}
 		}
 
-		editBuilder.insert(selection.end, `${generateBuildFunctions(structName)}`)
+		editBuilder.insert(selection.end, `${generateBuildFunctions(structName)}`);
 	});
 }
 
@@ -68,20 +74,20 @@ func Create${structName}(options ...${structName}Option) ${structName} {
 
 	return ${uncapitalizedStructName}
 }
-	`
+	`;
 }
 
 function generateOptionType(structName: string): string {
 	return `
 type ${structName}Option func(${structName}) ${structName}
-	`
+	`;
 }
 
 function generateWithFunction(structName: string, attribute: string, dataType: string): string {
 	const uncapitalizedStructName = structName[0].toLowerCase() + structName.slice(1);
 	const capitalizedAttribute = attribute[0].toUpperCase() + attribute.slice(1);
 	const uncapitalizedAttribute = attribute[0].toLowerCase() + attribute.slice(1);
-	const optionName = `${structName}Option`
+	const optionName = `${structName}Option`;
 	return `
 func With${capitalizedAttribute}(${uncapitalizedAttribute} ${dataType}) ${optionName} {
 	return func(${uncapitalizedStructName} ${structName}) ${structName} {
@@ -89,7 +95,7 @@ func With${capitalizedAttribute}(${uncapitalizedAttribute} ${dataType}) ${option
 		return ${uncapitalizedStructName}
 	}
 }
-	`
+	`;
 }
 
 // This method is called when your extension is deactivated
